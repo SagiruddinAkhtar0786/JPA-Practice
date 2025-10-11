93ca898b1622be796816b447f6884a6290c235b5
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-10-12 01:31:57 +0530

    commit

--- Fetch_data_from_the_public_API/src/main/java/com/practice/Fetch_data/FetchDataFromThePublicApiApplication.java
+++ Fetch_data_from_the_public_API/src/main/java/com/practice/Fetch_data/FetchDataFromThePublicApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FetchDataFromThePublicApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("start");
 		SpringApplication.run(FetchDataFromThePublicApiApplication.class, args);
 	}
 
