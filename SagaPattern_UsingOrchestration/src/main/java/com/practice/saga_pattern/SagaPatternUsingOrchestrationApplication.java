778c344ef7d3d2cfd4510e8302921e8f8ee20ccf
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-10-13 21:37:28 +0530

    commit

---
+++ SagaPattern_UsingOrchestration/src/main/java/com/practice/saga_pattern/SagaPatternUsingOrchestrationApplication.java
@@ -0,0 +1,13 @@
+package com.practice.saga_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPatternUsingOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaPatternUsingOrchestrationApplication.class, args);
+	}
+
+}
---
+++ SagaPattern_UsingOrchestration/src/test/java/com/practice/saga_pattern/SagaPatternUsingOrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.saga_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaPatternUsingOrchestrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
